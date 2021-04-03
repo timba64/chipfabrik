c684802987393faa425173751e4c30f07f5b1c1d
timba64 <timba64@mail.ru>
2021-04-03 13:15:17 +0300

    made in common

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function makeMenu() {
+    const mb = document.querySelector('.mobtn'),
+    hd = document.querySelector('.top-menu');
+
+    mb.addEventListener('click', function(){
+        this.classList.toggle('open');
+        hd.classList.toggle('open');
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  makeMenu();
+}, false);
